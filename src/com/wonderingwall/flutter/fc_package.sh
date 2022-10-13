ac38be85f13acf055af3462c4e28d9f37bdad166
zouzhangwei <wearecisco@gmail.com>
2022-10-13 18:53:06 +0800

    UPDATE: update flutter create shell to support app name and dir name.

--- src/com/wonderingwall/flutter/fc_package.sh
+++ src/com/wonderingwall/flutter/fc_package.sh
@@ -18,13 +18,33 @@ ORG=com.wonderingwall
 
 ## 检测参数，输入项目名称
 if [ -z "$1" ] ; then
-  echo "Type a package name [flutter_package_name]"
+  echo "Type a package name [flutter_package_name], it used for app_name, flutter package name in 'pubspec.yaml'."
 	read -r PROJECT_NAME
 else
 	echo "not null echo $1"
 	PROJECT_NAME=$1
 fi
 
-echo "We created project with name '$PROJECT_NAME'"
+## 是否输入目录名称，目录名称和项目名称可以不一致
+echo "Type a project dir name , if empty that ctreate dir of name is '[$PROJECT_NAME]'."
+read -r DIR
+if [ -z "$DIR" ] ; then
+    DIR_NAME=$PROJECT_NAME
+else
+    DIR_NAME=$DIR
+fi
 
-flutter create -t package --project-name "${PROJECT_NAME}" --org $ORG -a java -i swift "${PROJECT_NAME}"
+
+echo "We are creating a package project that name is '$PROJECT_NAME' in dir '$DIR_NAME'."
+
+flutter create -t package --project-name "${PROJECT_NAME}" --org $ORG -a java -i swift "${DIR_NAME}"
+
+
+## PROJECT_NAME 的定义直接被认定为项目的名称，app_name显示也是它，还有就是当package被引用,此Package的名字也是这个。
+
+
+## 创建项目
+## 1. 直接创建项目，给定项目名称即可，目录名称相同
+## 2. 指定模版创建项目，减少项目初始化编写时间。
+## 3. 指定package名，自动改写。第一次创建可，后续直接改
+## 4. 转化应用图片，启动图片等。
--- src/com/wonderingwall/flutter/fc_project.sh
+++ src/com/wonderingwall/flutter/fc_project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ****************************************************************************************
-#  Flutter 创建项目命令
+#  Flutter 创建app项目命令
 #  version    : @version@
 #  author     : @author@
 #  date       : @date@
@@ -18,13 +18,23 @@ ORG=com.wonderingwall
 
 ## 检测参数，输入项目名称
 if [ -z "$1" ] ; then
-  echo "Type a project name [flutter_app_name]"
+    echo "Type a app name [flutter_app_name], it used for app_name, flutter app name in 'pubspec.yaml'."
 	read -r PROJECT_NAME
 else
-	echo "not null echo $1"
+	echo "You typed project name : $1"
 	PROJECT_NAME=$1
 fi
 
-echo "We created project with name '$PROJECT_NAME'"
+## 是否输入目录名称，目录名称和项目名称可以不一致
+echo "Type a project dir name , if empty that ctreate dir of name is '[$PROJECT_NAME]'."
+read -r DIR
+if [ -z "$DIR" ] ; then
+    DIR_NAME=$PROJECT_NAME
+else
+    DIR_NAME=$DIR
+fi
 
-flutter create --project-name "${PROJECT_NAME}" --platforms android,ios --org $ORG "${PROJECT_NAME}"
+
+echo "We are creating a app project that name is '$PROJECT_NAME' in dir '$DIR_NAME'."
+
+flutter create --project-name "${PROJECT_NAME}" --platforms android,ios --org $ORG "${DIR_NAME}"
